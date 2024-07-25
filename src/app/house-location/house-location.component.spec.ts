4833db345d2ab45f7b79d6fc173180d1aaa3c06c
Kaushik Gs <101343215+kaushikgs123@users.noreply.github.com>
2024-07-25 15:56:53 +0530

    Add files via upload

---
+++ src/app/house-location/house-location.component.spec.ts
@@ -0,0 +1,20 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HousingLocationComponent } from './house-location.component';
+
+describe('HouseLocationComponent', () => {
+  let component: HousingLocationComponent;
+  let fixture: ComponentFixture<HousingLocationComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HousingLocationComponent]
+    });
+    fixture = TestBed.createComponent(HousingLocationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
